8709040cbbe2d2cbf571c1a180e2c3ef1df99835
Olha Teplova <olitieplove@gmail.com>
2024-01-11 19:19:49 +0200

    feat: complete 44 tests

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -551,8 +551,9 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  // throw new Error('Not implemented');
+  return arr.slice(-n).concat(arr.slice(0, -n));
 }
 
 /**
@@ -582,6 +583,7 @@ function sortDigitNamesByNumericOrder(arr) {
     eight: 8,
     nine: 9,
   };
+
   return arr.sort((a, b) => digits[a] - digits[b]);
 }
 
